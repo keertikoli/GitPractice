428958f559ae058b1a9dc831447d050219b37722
keertikoli <keertikoli2@gmail.com>
2022-09-28 15:10:38 +0530

    git commit

---
+++ src/test/java/github_practice_Test/TestCase1_github.java
@@ -0,0 +1,10 @@
+package github_practice_Test;
+
+import org.testng.annotations.Test;
+
+public class TestCase1_github {
+@Test
+public void githubTest() {
+	System.out.println("github is generated");
+}
+}
